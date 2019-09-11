dda31bf19329f2cf3b499e832f4d6e13295e141d
ryu022304 <ryu022304@gmail.com>
2019-09-11 23:57:16 +0900

    [update]グラフ大きさ調節

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,10 +1,10 @@
 <template>
     <b-card-group deck>
-        <b-card header="Graph Area">
+        <b-card header="Graph Area" id="graph">
             <v-stage :config="configKonva">
                 <v-layer>
-                    <v-shape :config="configLine">
-                    </v-shape>
+                    <v-shape :config="configAxisLine"></v-shape>
+                    <v-circle :config="configCircle"></v-circle>
                 </v-layer>
             </v-stage>
         </b-card>
@@ -12,28 +12,39 @@
 </template>
 
 <script>
+
+const width = document.getElementById('graph').clientWidth * 0.9;
+const height = document.getElementById('graph').clientHeight * 0.9;
+
 export default {
-  data() {
-    return {
-      configKonva: {
-        width: 500,
-        height: 200
-      },
-      configLine: {
-        sceneFunc: function(context) {
-            context.beginPath();
-            // 横軸
-            context.moveTo(100, 100);
-            context.lineTo(900, 100);
-            // 縦軸
-            context.moveTo(300, 20);
-            context.lineTo(300, 620);
-            context.stroke();
-            context.closePath();
-        },
-        stroke: 'black'
+    data() {
+        return {
+            configKonva: {
+                width: width,
+                height: height
+            },
+            configAxisLine: {
+                sceneFunc: function(context) {
+                    context.beginPath();
+                    // 横軸
+                    context.moveTo(width*0.1, height/2);
+                    context.lineTo(width*0.9, height/2);
+                    // 縦軸
+                    context.moveTo(width/2, height*0.1);
+                    context.lineTo(width/2, height*0.9);
+                    context.stroke();
+                }
+            },
+            configCircle: {
+                x: 200,
+                y: 100,
+                radius: 70,
+                fill: 'red',
+                stroke: 'black',
+                strokeWidth: 4,
+                draggable: true
+            }
         }
-      }
-  }
+    }
 };
 </script>
