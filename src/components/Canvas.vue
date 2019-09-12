7c0edf0999476a4dab4d305d886ed149bf837d32
ryu022304 <ryu022304@gmail.com>
2019-09-12 23:11:32 +0900

    [update]文字の表示

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,10 +1,10 @@
 <template>
     <b-card-group deck>
         <b-card header="Graph Area" id="graph">
-            <v-stage :config="configKonva">
-                <v-layer>
+            <v-stage :config="configKonva" ref="stage">
+                <v-layer ref="layer">
                     <v-shape :config="configAxisLine"></v-shape>
-                    <v-circle :config="configCircle"></v-circle>
+                    <v-text :config="configText"></v-text>
                 </v-layer>
             </v-stage>
         </b-card>
@@ -13,9 +13,10 @@
 
 <script>
 
-const width = document.getElementById('graph').clientWidth * 0.9;
-const height = document.getElementById('graph').clientHeight * 0.9;
-
+//const width = document.getElementById('graph').clientWidth * 0.9;
+//const height = document.getElementById('graph').clientHeight * 0.9;
+const width = 800;
+const height = 400;
 export default {
     data() {
         return {
@@ -35,13 +36,11 @@ export default {
                     context.stroke();
                 }
             },
-            configCircle: {
-                x: 200,
-                y: 100,
-                radius: 70,
-                fill: 'red',
-                stroke: 'black',
-                strokeWidth: 4,
+            configText: {
+                text: 'テスト',
+                x: width/2,
+                y: height/2,
+                fontSize: 24,
                 draggable: true
             }
         }
