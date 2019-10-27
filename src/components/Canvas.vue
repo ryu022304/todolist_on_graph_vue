af3dc190d0ca13b0e89a106b9ed4aef7f8d9de7b
ryu022304 <ryu022304@gmail.com>
2019-10-27 22:28:18 +0900

    [update]プレビューの表示位置調整機能追加

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,7 +1,8 @@
 <template>
     <b-card-group deck>
-        <b-modal id="my-modal"
+        <b-modal id="preview-modal"
          size="xl"
+         @hidden="resetModal"
         >
             <template v-slot:modal-title>
                 Screenshot Preview
@@ -9,8 +10,14 @@
             この画像をダウンロードしますか？ <br>
             ※画像が表示されない場合はRetryを押してください
             <div id="preview" class="text-center"></div>
-            <template v-slot:modal-footer>
-                <b-button class="float-right">
+            <template v-slot:modal-footer="{cancel}">
+                <b-input-group prepend="-X" append="+X" class="mt-3">
+                    <b-form-input type="range" min="-100" max="100" v-model="xScroll" @change="redrawPreview"></b-form-input>
+                </b-input-group>
+                <b-input-group prepend="-Y" append="+Y" class="mt-3">
+                    <b-form-input type="range" min="-100" max="100" v-model="yScroll" @change="redrawPreview"></b-form-input>
+                </b-input-group>
+                <b-button variant="secondary" class="float-right" @click="cancel()">
                     Cancel
                 </b-button>
                 <b-button variant="success" class="float-right" @click="takeScreenShot">
@@ -22,9 +29,17 @@
             </template>
         </b-modal>
         <b-card header="Graph Area">
-            <b-button variant="info" v-b-modal="'my-modal'">
-                <font-awesome-icon icon="camera" size="lg" @click="takeScreenShot" />
+            <b-button variant="info" v-b-modal="'preview-modal'">
+                <font-awesome-icon icon="download" size="lg" @click="takeScreenShot" />
             </b-button>
+            <!--
+            <b-button variant="info">
+                <font-awesome-icon :icon="['fab', 'twitter']" size="lg" @click="tweetWithImage" />
+            </b-button>
+            <b-button variant="info">
+                <font-awesome-icon :icon="['fab', 'facebook-f']" size="lg" @click="postFacebookWithImage" />
+            </b-button>
+            -->
             <v-stage :config="{
                 width: this.width,
                 height: this.height
@@ -83,6 +98,8 @@ export default {
         return {
             width: 0,
             height: 0,
+            xScroll: 0,
+            yScroll: 0,
             configAxisLine: {
                 sceneFunc: function(context) {
                     context.beginPath();
@@ -136,19 +153,58 @@ export default {
             this.height = document.getElementById('graph').clientHeight;
             this.width = document.getElementById('graph').clientWidth;
         },
+        // Modalのリセット
+        resetModal(){
+            this.xScroll = 0;
+            this.yScroll = 0;
+        },
         // canvasのスクリーンショットプレビュー
         takeScreenShot(){
             let graph = document.getElementById('graph');
-            if (document.getElementById('preview-canvas') != null){return;}
             html2canvas(graph,{
-                height: this.height+100, // 下が見切れてしまうことがあるので対応
+                height: this.height,
                 width: this.width
             }).then(function(canvas){
-                canvas.id = "preview-canvas"
-                document.getElementById('preview').appendChild(canvas);
+                // TODO: レスポンシブ対応の際の画像出力
+                /*
+                let dataURL = canvas.toDataURL('image/png');
+                let imgEl = document.createElement("img");
+                imgEl.id = "preview-canvas";
+                imgEl.src = dataURL;
+                document.getElementById('preview').appendChild(imgEl);
+                */
+                if (document.getElementById('preview-canvas') != null){
+                    document.getElementById('preview-canvas').remove();
+                    canvas.id = "preview-canvas"
+                    document.getElementById('preview').appendChild(canvas);
+                }
+                else{
+                    canvas.id = "preview-canvas"
+                    document.getElementById('preview').appendChild(canvas);
+                }
             }).catch(function(err){
                 alert(err);
             });
+            this.xScroll = 0;
+            this.yScroll = 0;
+        },
+        // プレビューのリアルタイム修正
+        redrawPreview: function(){
+            let graph = document.getElementById('graph');
+            if (document.getElementById('preview-canvas') != null){
+                html2canvas(graph,{
+                    height: this.height,
+                    width: this.width,
+                    scrollY: this.yScroll,
+                    scrollX: this.xScroll
+                }).then(function(canvas){
+                    canvas.id = "preview-canvas"
+                    document.getElementById('preview-canvas').remove();
+                    document.getElementById('preview').appendChild(canvas);
+                }).catch(function(err){
+                    alert(err);
+                });
+            }
         },
         // 画像のダウンロード
         downloadImage(){
@@ -157,7 +213,22 @@ export default {
             link.href = dataURL;
             link.download = "todo_graph.png";
             link.click();
+        },
+        /*
+        // Twitterへの投稿
+        tweetWithImage(){
+            console.log('clicked tweet button');
+            const siteURL = location.host;
+            //const siteURL = "http://test.com"
+            console.log(siteURL);
+            const url = `https://twitter.com/intent/tweet?text=test&url=${siteURL}`;
+            window.open(url, 'twitter');
+        },
+        // Facebookへの投稿
+        postFacebookWithImage(){
+            console.log('clicked facebook button');
         }
+        */
     },
     mounted() {
         this.height = document.getElementById('graph').clientHeight;
